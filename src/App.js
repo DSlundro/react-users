8a322c107ddc7065eef980e9bda6d8e8244a00bd
DSlundro <s.dragan.95@outlook.it>
2022-09-25 11:59:01 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+
+
+function App() {
+  return (
+    <section className="flex justify-center pt-8">
+      <Form />
+    </section>
+  );
+}
+
+export default App;
